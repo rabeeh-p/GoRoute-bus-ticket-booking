eafa51b35d1a63b51944540752039992d91943a2
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-02-03 12:15:24 +0530

    backend chat bug fixed

--- GoRoute-backend/goRoute_api_pjt/user_app/consumers.py
+++ GoRoute-backend/goRoute_api_pjt/user_app/consumers.py
@@ -44,6 +44,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message_content = text_data_json['message']
         user_id = text_data_json.get('user_id')
+        timestamp = text_data_json.get('timestamp')
         print(user_id)
 
         try:
@@ -53,33 +54,36 @@ class ChatConsumer(AsyncWebsocketConsumer):
             user = None
             print(f"User with ID {user_id} does not exist")  
         if user and self.chat_room:
-            await self.save_message(user, message_content)
+            await self.save_message(user, message_content,timestamp)
 
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
                     'type': 'chat_message',
                     'message': message_content,
-                    'user': user.username   
+                    'user': user.username   ,
+                    'timestamp': timestamp
                 }
             )
 
     async def chat_message(self, event):
         message = event['message']
         user = event['user']
+        timestamp = event.get('timestamp')
 
         await self.send(text_data=json.dumps({
             'message': message,
-            'user': user   
+            'user': user   ,
+            'timestamp': timestamp 
         }))
 
     @database_sync_to_async
-    def save_message(self, user, message_content):
+    def save_message(self, user, message_content,timestamp):
         print(f"Saving message: {message_content} from user {user.username} to room {self.chat_room.name}")  # Debug log
 
         if not message_content:
             raise ValueError("Message content cannot be empty!")
-        message = Message.objects.create(user=user, room=self.chat_room, message=message_content)
+        message = Message.objects.create(user=user, room=self.chat_room, message=message_content,timestamp=timestamp)
         return message
 
 
